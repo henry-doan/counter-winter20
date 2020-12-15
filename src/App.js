84def6c640e616aba5c51daa2be370172665f7ad
Henry Doan <henrydoan96@gmail.com>
2020-12-14 19:43:46 -0700

    first counter done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+class App extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = { counterValue: 0 }
+  //   this.inc = this.inc.bind(this)
+  // }
+  state = { counterValue: 0 }
+
+  // inc() {
+  //   this.state.counterValue++
+  // }
+
+  inc = () => {
+    this.setState({ counterValue: this.state.counterValue + 1 })
+  }
+
+  dec = () => {
+    this.setState({ counterValue: this.state.counterValue - 1 })
+  }
+
+  // dec = () => {
+  //   this.setState( (state) => {
+  //     return { counterValue: state.counterValue -1 }
+  //   })
+  // }
+  // dec = () => {
+  //   const { counterValue } = this.state
+  //   this.setState({ counterValue: counterValue -1 })
+  // }
+
+  render() {
+    return (
+      <>
+        <h1>Counter Example</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {this.state.counterValue}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <button onClick={this.inc}>+</button>
+        <button onClick={this.dec}>-</button>
+      </>
+    )
+  }
 }
 
 export default App;
